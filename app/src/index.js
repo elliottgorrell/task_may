222070e43664bac2de938d788aae2d8dd427def2
Elliott Gorrell <elliottgorrell1@gmail.com>
2018-05-13 22:45:21 +1000

    created node app which can connect to db container (Had trouble getting connectivity between containers to work for awhile, turned out I fixed it 10 minutes ago however was only connecting on second attempt as db hadn’t yet initialised, sleep time I think)

--- app/src/index.js
+++ app/src/index.js
@@ -1,1 +1,9 @@
 const mongoose = require('mongoose');
+
+//db config
+const mongoDB = 'mongodb://database:27017/elliott';
+mongoose.connect(mongoDB, null)
+    .catch(err => {
+        console.log(err)
+    });
+const db = mongoose.connection;
