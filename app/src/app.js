f01a025add31284ab94debbf161c9326540a1193
Elliott Gorrell <elliottgorrell1@gmail.com>
2018-05-14 09:35:32 +1000

    enabled linting using airbnb style guide

--- app/src/app.js
+++ app/src/app.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.json({ limit: '1mb' }));
 
 // Bootstrap the application routes
 require('./routes')(app);
 
-app.listen(80, () => console.log('Example app listening on port 80') );
+app.listen(80, () => console.log('Example app listening on port 80'));
 
 module.exports = app;
--- app/src/index.js
+++ app/src/index.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const app = require('./app');
+const { log } = require('./utils.js');
 
-//db config
+// db config
 const mongoDB = 'mongodb://database:27017/elliott';
 mongoose.connect(mongoDB, null)
-    .catch(err => {
-        console.log(err)
-    });
+  .catch((err) => {
+    console.log(err);
+  });
 const db = mongoose.connection;
