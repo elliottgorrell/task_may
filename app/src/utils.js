8c5fcad01bb148d27a0206e2e41bacf23517ab8a
Elliott Gorrell <elliottgorrell1@gmail.com>
2018-05-14 09:14:21 +1000

    added express and logging

---
+++ app/src/utils.js
@@ -0,0 +1,11 @@
+const morgan = require('morgan');
+const tracer = require('tracer');
+
+const log = (() => {
+  const logger = tracer.colorConsole();
+  logger.requestLogger = morgan('dev');
+  return logger;
+})();
+
+
+module.exports = { log };
