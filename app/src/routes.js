bcc7d79741c828e0a0daac02a29b6a9da0715a33
Elliott Gorrell <elliottgorrell1@gmail.com>
2018-05-14 10:22:56 +1000

    struggling to find the record in database using mongoose so doing some wacky stuff to see whats happening

--- app/src/routes.js
+++ app/src/routes.js
@@ -1,7 +1,17 @@
 const { log } = require('./utils');
+const Page = require('./db/models/page');
 
 module.exports = (app) => {
-  app.get('/api/demo', async (req, res) => {
-    return res.status(500).json({ data: 'hey there' });
+  app.get('/', async (req, res) => {
+    Page.find({route: '/^\//'}, (err, pages) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/html' });
+        return res.end('<html><body>Couldn\'t find the page</body></html>');
+      } else {
+        log.info(`Length of pages ${pages.length}`);
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        return res.end('<html><body>hi there</body></html>');
+      }
+    });
   });
 };
